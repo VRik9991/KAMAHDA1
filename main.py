766cb37e151999f3397f6e7dcf40b8384f8ddf96
VRik <vrik9449@gmail.com>
2025-01-22 21:26:54 +0300

    sdfghj

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ with open("task.txt", "r") as file:
     for i in range(len(oskars)):
         pomp = oskars[i].split(",")
         oskars[i] = pomp
-    oskars = sorted(oskars, key=lambda X: X[1])
-    print(oskars[0][3])
-    print(oskars[0][4])
+    # oskars = sorted(oskars, key=lambda X: X[1])
+    # print(oskars[0][3])
+    # print(oskars[0][4])
+    for i in range(len(oskars)):  # Цыкл i = сч, лен(оскарс) = количевство елементов
+        if oskars[i][3][2] == "P" and oskars[i][4][2] == "T":  # проверяем что име на Р а фильм на Т, 2 = п.б.И.и.Ф.
+            print(oskars[i][3])  # Име 3 = ИМЕ
+            print(oskars[i][4])  # фильм 4 = ФИЛЬМ
