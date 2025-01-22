b8bf01483e52019dad6e4a4d5c2902ed55c91039
VRik <vrik9449@gmail.com>
2025-01-22 21:29:29 +0300

    da blin

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ with open("task.txt", "r") as file:
     # oskars = sorted(oskars, key=lambda X: X[1])
     # print(oskars[0][3])
     # print(oskars[0][4])
-    for i in range(len(oskars)):  # Цыкл i = сч, лен(оскарс) = количевство елементов
-        if oskars[i][3][2] == "P" and oskars[i][4][2] == "T":  # проверяем что име на Р а фильм на Т, 2 = п.б.И.и.Ф.
-            print(oskars[i][3])  # Име 3 = ИМЕ
-            print(oskars[i][4])  # фильм 4 = ФИЛЬМ
+    for i in range(len(oskars)):
+        if oskars[i][3][2] == "P" and oskars[i][4][2] == "T":
+            print(oskars[i][3])
+            print(oskars[i][4])
